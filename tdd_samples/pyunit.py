f2e7398c25968affa28744e1c13e3bbc1ebe4ea0
Grigoriy Beziuk <gbezyuk@gmail.com>
2012-04-14 14:56:46 +0400

    pylint usage tested, sample pyunit refactored

--- tdd_samples/pyunit.py
+++ tdd_samples/pyunit.py
@@ -1,19 +1,38 @@
+"""
+Sample pyunit tests
+"""
+
 import unittest
 
 # Here's our "unit".
-def IsOdd(n):
-    return n % 2 == 1
+def is_odd(number):
+    """
+    Returns true or false basing on parameter is odd or even
+    """
+    return number % 2 == 1
 
 # Here's our "unit tests".
 class IsOddTests(unittest.TestCase):
+    """
+    Unit tests for IsOdd class
+    """
 
-    def testOne(self):
-        self.failUnless(IsOdd(1))
+    def test_one(self):
+        """
+        One shoud be odd
+        """
+        self.failUnless(is_odd(1))
 
-    def testTwo(self):
-        self.failIf(IsOdd(2))
+    def test_two(self):
+        """
+        Two should be tested as even
+        """
+        self.failIf(is_odd(2))
 
 def main():
+    """
+    Test-running procedure
+    """
     unittest.main()
 
 if __name__ == '__main__':
